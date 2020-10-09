99a936d6dd759ad64e37f208e555017dbd5e116e
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-10-09 18:29:12 +0200

    cd..

--- src/main/kotlin/com/example/SimpleControllerKotlin/SimpleController.kt
+++ src/main/kotlin/com/example/SimpleControllerKotlin/SimpleController.kt
@@ -1,4 +1,12 @@
 package com.example.SimpleControllerKotlin
 
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
 class SimpleController {
+    @RequestMapping("")
+    fun info():String{
+        return "info 12345"
+    }
 }
