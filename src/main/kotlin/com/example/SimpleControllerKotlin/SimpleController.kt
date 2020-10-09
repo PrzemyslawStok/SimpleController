0247b44cfb02c17b127da69e817cea6dbaa5ef88
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-10-09 18:47:31 +0200

    cd..

--- src/main/kotlin/com/example/SimpleControllerKotlin/SimpleController.kt
+++ src/main/kotlin/com/example/SimpleControllerKotlin/SimpleController.kt
@@ -1,6 +1,7 @@
 package com.example.SimpleControllerKotlin
 
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -8,5 +9,14 @@ class SimpleController {
     @RequestMapping("")
     fun info():String{
         return "info 12345"
+    }
+    @RequestMapping("/next")
+    fun nextFun():Double{
+        return 2.0
+    }
+    @RequestMapping("linearFunction")
+    //y = a*x + b
+    fun linearFunction(@RequestParam("x") x:Double):Double{
+        return 5*x
     }
 }
